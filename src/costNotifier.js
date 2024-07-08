f8009e458cd005677200873dda9a3a9f4398d143
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2024-07-08 09:05:35 +0900

    ✨ ローカルでSlack通知が出るようにする

--- src/costNotifier.js
+++ src/costNotifier.js
@@ -1,7 +1,24 @@
 import { IncomingWebhook } from "@slack/webhook";
+import moment from "moment";
+import AWS from "aws-sdk";
 
 export async function handler(event, context) {
+  const now = moment();
+  const start = now.format("YYYY-MM-01");
+  const end = now.add(1, "month").format("YYYY-MM-01");
+  const ce = new AWS.CostExplorer({ region: "us-east-1" });
+  const constAndUsage = await ce
+    .getCostAndUsage({
+      TimePeriod: {
+        Start: start,
+        End: end,
+      },
+      Granularity: "MONTHLY",
+      Metrics: ["UnblendedCost"],
+    })
+    .promise();
+  const usdCost = constAndUsage.ResultsByTime[0].Total.UnblendedCost.Amount;
   const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
   const slackWebhook = new IncomingWebhook(slackWebhookUrl);
-  await slackWebhook.send("Hello Slack Webhook!");
+  await slackWebhook.send(`今月のAWS利用料金: $${usdCost}`);
 }
